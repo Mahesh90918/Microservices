e3be3eb888a088fcd20ce149b520ba91dc24741d
Mahesh90918 <mahesh996345@gmail.com>
2023-03-04 18:55:34 +0530

    CircuitBreaker With Resilience4J

---
+++ 7.2User-Service-CircuitBreaker-Resonace4j/src/main/java/com/microServices/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.microServices.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microServices.dto.ResponseDto;
+import com.microServices.entity.User;
+import com.microServices.service.UserService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+
+@RestController
+@RequestMapping("api/users")
+//@AllArgsConstructor
+public class UserController {
+	
+	@Autowired
+	@Lazy
+    private UserService userService;
+
+    private  static final  String DEPT_SERVICE="myProjectAllRemotecallsDepartMent";
+    
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        User savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+   
+    @GetMapping("{id}")
+    @CircuitBreaker(name =DEPT_SERVICE ,fallbackMethod = "getApiFallBack")
+    public ResponseEntity<ResponseDto> getUser(@PathVariable("id") Long userId){
+        ResponseDto responseDto = userService.getUser(userId);
+        return ResponseEntity.ok(responseDto);
+    }
+    
+    // both return Types are Equal Must
+    public ResponseEntity<ResponseDto>  getApiFallBack(Exception ex) {
+		return new ResponseEntity("Error : Department Service is Down",HttpStatus.OK);
+    	
+    }
+    
+  
+}
